5d343f2996117594397297a08430ac127c4f2da0
knikolov <kami_985@abv.bg>
2022-01-13 03:16:03 +0200

    Commit to attempt and fix the failing test

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div style = {{
+    <div className = "App" style = {{
       maxWidth: "60rem",
       margin: "2rem auto",
       padding: "1rem",
       boxShadow: "0 2px 8px rgba(0, 0, 0, 0.25)",
       borderRadius: "14px",
-      backgroundColor: "white"
+      backgroundColor: "white",
     }}>
       <div style ={
         {
@@ -16,17 +16,17 @@ function App() {
           justifyContent: "space-between",
           margin: "1rem",
           paddingBottom: "1rem",
-          borderBottom: "1px solid #ccc"
+          borderBottom: "1px solid #ccc",
         }
       }>
         <h3 style = {
           {
-            margin: "0 0 0.25rem 0"
+            margin: "0 0 0.25rem 0",
           }
         }>Sushi</h3>
         <div style = {
           {
-            fontStyle: "italic"
+            fontStyle: "italic",
           }
         }>I prefer steak than sushi.</div>
         <div style = {
@@ -34,7 +34,7 @@ function App() {
   marginTop: "0.25rem",
   fontWeight: "bold",
   color: "#ad5502",
-  fontSize: "1.25rem"
+  fontSize: "1.25rem",
           }
 }>$9.99</div>
       </div>
