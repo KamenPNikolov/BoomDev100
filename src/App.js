4debc7283eec2fc95a7f860cb01b903cc53e335e
knikolov <kami_985@abv.bg>
2022-01-13 11:07:19 +0200

    Creating an object of js objects to cover style

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className = "App" style = {{
+  const styles = {
+    root:{
       maxWidth: "60rem",
       margin: "2rem auto",
       padding: "1rem",
       boxShadow: "0 2px 8px rgba(0, 0, 0, 0.25)",
       borderRadius: "14px",
-      backgroundColor: "white",
-    }}>
-      <div style ={
-        {
-          display: "flex",
-          justifyContent: "space-between",
-          margin: "1rem",
-          paddingBottom: "1rem",
-          borderBottom: "1px solid #ccc",
-        }
-      }>
-        <h3 style = {
-          {
-            margin: "0 0 0.25rem 0",
-          }
-        }>Sushi</h3>
-        <div style = {
-          {
-            fontStyle: "italic",
-          }
-        }>I prefer steak than sushi.</div>
-        <div style = {
-          {
-  marginTop: "0.25rem",
-  fontWeight: "bold",
-  color: "#ad5502",
-  fontSize: "1.25rem",
-          }
-}>$9.99</div>
+      backgroundColor: "white"
+    },
+    wrapper:{ 
+      display: "flex",
+      justifyContent: "space-between",
+      margin: "1rem",
+      paddingBottom: "1rem",
+      borderBottom: "1px solid #ccc",
+   }, 
+   mealHeading: {
+    margin: "0 0 0.25rem 0",
+  },
+  description: {
+    fontStyle: "italic",
+  },
+  price: {
+    marginTop: "0.25rem",
+    fontWeight: "bold",
+    color: "#ad5502",
+    fontSize: "1.25rem",
+  } 
+  }
+  return (
+    <div className = "App" style = {styles.root}>
+      <div style ={styles.wrapper}>
+        <h3 style = { styles.mealHeading}>Sushi</h3>
+        <div style = { styles.description}>I prefer steak than sushi.</div>
+        <div style = {styles.price}>$9.99</div>
       </div>
     </div>
   );
