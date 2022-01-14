85b669bf654d6f03513f933565b04f311a76c104
knikolov <kami_985@abv.bg>
2022-01-14 09:36:10 +0200

    Another fix to make the test work

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import "./App.css";
 
 function App() {
-  const styles = {
-    root:{
-      maxWidth: "60rem",
-      margin: "2rem auto",
-      padding: "1rem",
-      boxShadow: "0 2px 8px rgba(0, 0, 0, 0.25)",
-      borderRadius: "14px",
-      backgroundColor: "white"
-    },
-    wrapper:{ 
-      display: "flex",
-      justifyContent: "space-between",
-      margin: "1rem",
-      paddingBottom: "1rem",
-      borderBottom: "1px solid #ccc",
-   }, 
-   mealHeading: {
-    margin: "0 0 0.25rem 0",
-  },
-  description: {
-    fontStyle: "italic",
-  },
-  price: {
-    marginTop: "0.25rem",
-    fontWeight: "bold",
-    color: "#ad5502",
-    fontSize: "1.25rem",
-  } 
-  }
+  
+  
   return (
     <div className = "App" style={{
       maxWidth: "60rem",
